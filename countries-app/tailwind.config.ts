b3262d69c6fc5f78d8116d497d5cd1bb8e0b6925
Aya-Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2023-08-31 19:45:31 +0300

    custome colors in tailwind

--- countries-app/tailwind.config.ts
+++ countries-app/tailwind.config.ts
@@ -7,7 +7,14 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'customeBG':'#141E46',
+        'customeCard':'#FFF3DA',
+        'focusedText':'#F6635C',
+        'countryText': '#F7E987'
+      }
+    },
   },
   plugins: [],
 }
